84a23486ec5fa4d01bca75a6543ce4cbad278f2a
nirmal56 <nirmal.patel@einfochips.com>
2022-05-23 14:56:21 +0530

    video open

--- day1/basic.cpp
+++ day1/basic.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
-#include"/usr/local/include/opencv4/opencv2/core/core.hpp"
+// #include"/usr/local/include/opencv4/opencv2/core/core.hpp"
 #include"/usr/local/include/opencv4/opencv2/opencv.hpp"
+// #include<opencv4/opencv2/highgui/highgui.hpp>
 using namespace std;
 using namespace cv;
 int main(){
     Mat img;
-	img=imread("cheta.png");
-	imshow("Input", img);
+	img=imread("cheta.png",IMREAD_GRAYSCALE);
+    // cv2.resize
+    namedWindow("mywindow",WINDOW_NORMAL);
+	imshow("mywindow", img);
 	waitKey(0);
+    // destroyWindow("mywindow");
+    // img=imread("cheta.png",IMREAD_COLOR);
+	// imshow("Input1", img);
+	// waitKey(0);
+    // destroyWindow("input1");
 	return 0;
 }
---
+++ day1/video.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include"/usr/local/include/opencv4/opencv2/opencv.hpp"
+#include"/usr/local/include/opencv4/opencv2/highgui/highgui.hpp"
+using namespace std;
+using namespace cv;
+int main(){
+    int WIDTH = 1000, HEIGHT = 500;
+    Mat frame;
+    VideoCapture capture;
+    capture.open("video.mp4");
+    for(;;){
+        capture>>frame;
+        namedWindow("video", WINDOW_NORMAL);
+        // resizeWindow("video", WIDTH, HEIGHT);
+        imshow("video",frame);
+        if(waitKey(33)>0){break;}
+        
+    }
+	return 0;
+}
